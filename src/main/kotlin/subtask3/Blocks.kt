e67e3a9a50cd8c6a7bf3ed42f9bac33ab547be52
AkexanderNovik <hyper_alex@mail.ru>
2021-06-12 17:47:49 +0300

    unit: subtask 3 implementation

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,19 +1,62 @@
 package subtask3
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
         return when (blockB.simpleName) {
-            "String" -> {var z = StringBuffer()
-                blockA.filterIsInstance<String>()
-                    .forEach{z.append(it)}
-                z}
-            "Int" ->  blockA.filterIsInstance<Int>().sum()
-            "LocalDate" -> blockA.filterIsInstance<LocalDate>()
+            "String" -> {
+                val z = StringBuffer()
+                blockA.filterIsInstance<String>().forEach { z.append(it) }
+                return z
+            }
+            "Int" -> blockA.filterIsInstance<Int>().sum()
+            "LocalDate" -> findClosestDate(blockA)
             else -> "Incorrect filter type"
+        }
+    }
+}
+
+private fun dateToInt(date: LocalDate): Int {
+    return date.format(DateTimeFormatter.ofPattern("yyyyMMdd")).toString().toInt()
+}
+
+private fun findClosestDate(blockA: Array<*>): String {
+
+    val sortedSet = blockA.filterIsInstance<LocalDate>().toSortedSet()
+
+    return when {
+        sortedSet.isEmpty() -> "blockA is empty"
+        sortedSet.size == 1 -> sortedSet.first().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            .toString()
+        else -> {
+            val currentDay = LocalDate.now()
+            lateinit var result: LocalDate
+
+            sortedSet.add(currentDay)
+
+            when (currentDay) {
+                sortedSet.last() -> {
+                    result = sortedSet.elementAt(sortedSet.size - 2)
+                }
+                sortedSet.first() -> {
+                    result = sortedSet.elementAt(1)
+                }
+                else -> {
+                    val currentDayIndex = sortedSet.indexOf(currentDay)
+                    val closestPastDate = sortedSet.elementAt(currentDayIndex - 1)
+                    val closestFutureDate = sortedSet.elementAt(currentDayIndex + 1)
+
+                    result = if (Math.abs(dateToInt(closestPastDate) - dateToInt(currentDay)) <
+                        Math.abs(dateToInt(closestFutureDate) - dateToInt(currentDay))
+                    ) closestPastDate else closestFutureDate
+                }
+
+            }
+            result.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")).toString()
         }
     }
 }
